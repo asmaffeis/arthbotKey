bb844fce9549cbc08eebe3fd063669316bbf7369
asmaffeis <arthur.maffeis@arthwind.com.br>
2024-08-01 11:16:49 -0300

    testegpio

--- keyboard.py
+++ keyboard.py
@@ -1,15 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-BUTTON_GPIO = 27  # Use um pino GPIO diferente para teste
+# Configuração do GPIO
+BUTTON_GPIO = 27  # Escolha um pino GPIO disponível
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+print("Aguardando pressionamento do botão...")
+
 try:
     while True:
+        # Verifique o estado do botão
         if GPIO.input(BUTTON_GPIO) == GPIO.HIGH:
             print("Botão pressionado!")
+            # Aguarde enquanto o botão estiver pressionado
+            while GPIO.input(BUTTON_GPIO) == GPIO.HIGH:
+                time.sleep(0.1)
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Programa encerrado pelo usuário")
