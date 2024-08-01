5687a15e524465aa032f56d9ecb69cc3af08b18c
asmaffeis <arthur.maffeis@arthwind.com.br>
2024-08-01 11:19:44 -0300

    edgeadd

--- keyboard.py
+++ keyboard.py
@@ -1,22 +1,27 @@
 import RPi.GPIO as GPIO
+from pynput.keyboard import Controller
 import time
 
 # Configuração do GPIO
-BUTTON_GPIO = 27  # Escolha um pino GPIO disponível
+BUTTON_GPIO = 27  # Use o mesmo pino GPIO que funcionou antes
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-print("Aguardando pressionamento do botão...")
+# Inicializa o controlador de teclado
+keyboard = Controller()
+
+def button_callback(channel):
+    print("Botão pressionado - Simulando tecla 'p'")
+    keyboard.press('p')
+    keyboard.release('p')
+
+# Adiciona detecção de borda com a função de callback
+GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=button_callback, bouncetime=200)
 
 try:
+    print("Aguardando pressionamento do botão para simular a tecla 'p'...")
     while True:
-        # Verifique o estado do botão
-        if GPIO.input(BUTTON_GPIO) == GPIO.HIGH:
-            print("Botão pressionado!")
-            # Aguarde enquanto o botão estiver pressionado
-            while GPIO.input(BUTTON_GPIO) == GPIO.HIGH:
-                time.sleep(0.1)
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Programa encerrado pelo usuário")
